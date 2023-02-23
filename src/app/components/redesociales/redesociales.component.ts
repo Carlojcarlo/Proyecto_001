270074a7966096cf6ce9bceee1ef2434f480fab9
Carlo Feli <carloju@gamil.com>
2023-02-23 18:13:26 -0300

    commit 02

---
+++ src/app/components/redesociales/redesociales.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-redesociales',
+  templateUrl: './redesociales.component.html',
+  styleUrls: ['./redesociales.component.css']
+})
+export class RedesocialesComponent {
+
+}
