eb6b063782f37c6467391e956126d09985556d65
Carlo Feli <carloju@gamil.com>
2023-04-29 14:32:43 -0300

    commit 8

---
+++ back-End/src/main/java/com/portifolio/Feli/FeliApplication.java
@@ -0,0 +1,13 @@
+package com.portifolio.Feli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeliApplication.class, args);
+	}
+
+}
---
+++ back-End/src/test/java/com/portifolio/Feli/FeliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.Feli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
